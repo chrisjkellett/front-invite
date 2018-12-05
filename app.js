c96a4a594d11337ff5b81be8f0c21fe549dbd686
cjkellett <cjkellett@hotmail.co.uk>
2018-12-05 13:37:22 +0100

    basic routing

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get('/', (request, response) => {
+  response.render('index');
+});
+
+app.post('/', (req, res) => {
+  res.redirect('/invitation');
+});
+
+app.get('/invitation', (request, response) => {
+  response.send('invitations');
+});
+
+
+app.listen(3000);
+
