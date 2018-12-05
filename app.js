927a24f57d425458e2ca74c9b520cab88b1763b2
cjkellett <cjkellett@hotmail.co.uk>
2018-12-05 14:25:12 +0100

    added url encoded

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded());
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (request, response) => {
-  response.render('index');
+
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-app.post('/', (req, res) => {
-  res.redirect('/invitation');
+
+app.post('/invite', (req, res) => {
+  res.send({
+    title: req.body
+  })
 });
 
-app.get('/invitation', (request, response) => {
-  response.send('invitations');
-});
+// app.get('/invitation', (request, response) => {
+//   response.send('invitations');
+// });
 
 
 app.listen(3000);
