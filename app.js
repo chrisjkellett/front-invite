a2143ed367608c08dd758b884f4461f87266d0ff
cjkellett <cjkellett@hotmail.co.uk>
2018-12-07 10:42:20 +0100

    added invitations page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.urlencoded());
+
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use(express.static(__dirname + '/public'));
@@ -13,14 +14,13 @@ app.get('/', (req, res) => {
 
 
 app.post('/invite', (req, res) => {
-  res.send({
-    title: req.body
-  })
+  const {title, description} = req.body;
+  res.redirect(`/invitation/?title=${title}&description=${description}`);
 });
 
-// app.get('/invitation', (request, response) => {
-//   response.send('invitations');
-// });
+app.get('/invitation/', (req, res) => {
+  res.render('invitation', {...req.query})
+});
 
 
 app.listen(3000);
