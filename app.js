00292bca1dd7547806b087e4617d1f3554ca1324
cjkellett <cjkellett@hotmail.co.uk>
2018-12-07 11:20:25 +0100

    added basic styles

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: true}));
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
